49566ada33d5d5b79be15ce671b62fb86acf4f1b
Lucas Oliveira <lucasouliveira@gmail.com>
2022-08-04 00:07:36 -0300

    Revert "readd exception handler"
    This reverts commit 35c265cc6ccca22878fd7ab05ff800b67f1497ce.

--- controller/userController.ts
+++ controller/userController.ts
@@ -1,39 +1,50 @@
 import { OpineRequest, OpineResponse } from 'https://deno.land/x/opine@2.2.0/mod.ts';
 import userService from '../service/userService.ts';
-import exceptionHandler from './util/exceptionHandler.ts';
 
 export default {
-  register: (req: OpineRequest, res: OpineResponse) => {
+  register: async (req: OpineRequest, res: OpineResponse) => {
     const { user, password } = req.body;
-    exceptionHandler(res, async () => {
-      if(await userService. register(user, password)){
+    try {
+      if (await userService.register(user, password)) {
         res.status = 201;
         res.json({ message: `user ${user} registered sucessfully` });
       }
       res.status = 400;
       res.json({ message: 'no user was persisted!' });
-    });
+    } catch (err) {
+      console.error(err);
+      res.status = 500;
+      res.json({ error: err?.message ?? err });
+    }
   },
-  login: (req: OpineRequest, res: OpineResponse) => {
+  login: async (req: OpineRequest, res: OpineResponse) => {
     const { user, password } = req.body;
-    exceptionHandler(res, async () => {
+    try {
       if (await userService.login(user, password)) {
         res.status = 204;
         res.send();
       }
       res.status = 400;
       res.json({ message: 'user or password invalid!' });
-    });
+    } catch (err) {
+      console.error(err);
+      res.status = 500;
+      res.json({ error: err?.message ?? err });
+    }
   },
-  unregister: (req: OpineRequest, res: OpineResponse) => {
+  unregister: async (req: OpineRequest, res: OpineResponse) => {
     const { user, password } = req.body;
-    exceptionHandler(res, async () => {
+    try {
       if (await userService.unregister(user, password)) {
         res.status = 204;
         res.send();
       }
       res.status = 400;
       res.json({ message: 'user or password invalid!' });
-    });
+    } catch (err) {
+      console.error(err);
+      res.status = 500;
+      res.json({ error: err?.message ?? err });
+    }
   }
 }
