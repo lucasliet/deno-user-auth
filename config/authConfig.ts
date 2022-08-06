be8f084fec860e2877c453bcb838cdf3b48a3057
lucasliet <lucasouliveira@gmail.com>
2022-08-06 03:46:39 -0300

    fix nullable secret to local test

--- config/authConfig.ts
+++ config/authConfig.ts
@@ -3,7 +3,7 @@ import { create, getNumericDate } from "https://deno.land/x/djwt@v2.7/mod.ts";
 const ONE_DAY = getNumericDate(3600 * 24)
 
 const key = await crypto.subtle.importKey('jwk',
-  { kty: 'oct', k: Deno.env.get('JWS_SECRET_TOKEN') },
+  { kty: 'oct', k: Deno.env.get('JWS_SECRET_TOKEN') ?? 'secret' },
   { name: "HMAC", hash: "SHA-512" }, true, ["sign", "verify"]);
 
 export const createUserToken = (user: string) =>
