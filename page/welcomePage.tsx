827f2e0adfd225dc4df510a38fe3cd8d110f4789
lucasliet <lucasouliveira@gmail.com>
2022-08-06 03:47:15 -0300

    add JSX home page

---
+++ page/welcomePage.tsx
@@ -0,0 +1,28 @@
+/** @jsx h */
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+import { h, renderSSR, withStyles } from "https://deno.land/x/nano_jsx@v0.0.33/mod.ts";
+
+const css = new TextDecoder().decode(await Deno.readFile('./page/styles.css'));
+
+function WelcomePage() {
+  return withStyles(css)(
+    <html>
+      <head>
+        <title>Deno Middleware</title>
+      </head>
+      <body>
+        <main>
+          <h1>To start please check the Insomnia template at <a href="https://github.com/lucasliet/deno-middleware">our Github Page</a></h1>
+          <h2>or download it <a href="insomnia://app/import?uri=https://raw.githubusercontent.com/lucasliet/deno-middleware/master/.github/Insomnia_2022-08-06.yaml">here</a></h2>
+        </main>
+      </body>
+    </html>
+  );
+}
+
+export default function render() {
+  return renderSSR(<WelcomePage />)
+}
